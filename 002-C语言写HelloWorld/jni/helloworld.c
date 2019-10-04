2656201f402c10e1de06e4242d0364cb066c8cd6
iicey <1525164488@qq.com>
2019-10-04 23:46:42 +0800

    The first day

---
+++ 002-C语言写HelloWorld/jni/helloworld.c
@@ -0,0 +1,11 @@
+//include是预处理指令，用来引入头文件 
+#include <stdio.h>
+
+//程序入口函数main 
+int main(int argc,int**argv[]) 
+{
+ printf("HelloWorld!\n");
+ //system("pause");
+ return 0;
+}
+
---
+++ 006-常量/练习.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+# define PI 3.14
+int main()
+{
+    printf("%f\n", PI);
+
+	int a;
+	a = 23*8;
+	printf("%d\n", a);
+	system("pause");
+	return 0;
+}
